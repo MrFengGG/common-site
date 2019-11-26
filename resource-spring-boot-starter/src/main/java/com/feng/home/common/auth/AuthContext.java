d231df3594dcf5b60e75f7e643c9fafd101c972f
unknown <fengziyu@shangquanquan.com>
2019-11-26 18:06:36 +0800

    bugfix

--- resource-spring-boot-starter/src/main/java/com/feng/home/common/auth/AuthContext.java
+++ resource-spring-boot-starter/src/main/java/com/feng/home/common/auth/AuthContext.java
@@ -16,4 +16,5 @@ public class AuthContext {
     public static void clean(){
         contextUserThreadLocal.remove();
     }
+    
 }
--- resource-spring-boot-starter/src/main/java/com/feng/home/common/resource/aspect/ResourceAspect.java
+++ resource-spring-boot-starter/src/main/java/com/feng/home/common/resource/aspect/ResourceAspect.java
@@ -23,10 +23,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Aspect
 public class ResourceAspect {
